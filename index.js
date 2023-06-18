30140d1f02d66264ad3f959dae9cf2a89d1418e3
gift kimani <gift.kimani@students.moringaschool.com>
2023-06-18 17:42:12 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+ function destructivelyAppendCat(){
+    cats.push("Ralph")
+    return destructivelyAppendCat;
+ }
+ function destructivelyPrependCat(){
+    cats.unshift("Bob");
+    return destructivelyPrependCat;
+ }
+ function destructivelyRemoveLastCat(){
+    cats.pop();
+    return destructivelyRemoveLastCat;
+ }
+ function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return destructivelyRemoveFirstCat;
+ }
+ function appendCat(){
+const copyOfCats = [...cats, "Broom"];
+return copyOfCats;
+ }
+ function prependCat(){
+    const newOfCats = ["Arnold", ...cats];
+    return newOfCats;
+ }
+ function removeLastCat(){
+   const slicedOfCats =  cats.slice(0 , 2);
+    return slicedOfCats;
+ }
+ function removeFirstCat(){
+    const slicedFirstCats = cats.slice(1);
+    return slicedFirstCats;
+ }
